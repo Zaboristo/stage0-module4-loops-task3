bbbd2b4609d55e2ec25192f37887a95057667122
boris <baizenbe@gmail.com>
2023-07-04 16:59:25 +0600

    repo

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -1,12 +1,18 @@
 package school.mjc.stage0.loops.task3;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
         int res = 0;
         for(int i = 0; i<lengthOfLastNumber; i++){
             int count = 0;
-
+            for (int j = 0; j<=i; j++){
+                count = (count*10) + 9;
             }
+            res += count;
+            }
+        System.out.println(res);
         }
+
     }
-}
